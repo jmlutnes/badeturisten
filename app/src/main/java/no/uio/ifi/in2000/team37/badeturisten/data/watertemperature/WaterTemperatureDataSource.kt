281e275f8958009e46e1bc8d0efa2739d3b209a7
Adam Giæver <adamng@ulrik.uio.no>
2024-03-15 13:56:03 +0100

    rydder opp i konflikt

--- app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/data/watertemperature/WaterTemperatureDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/data/watertemperature/WaterTemperatureDataSource.kt
@@ -5,7 +5,7 @@ import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
 import io.ktor.serialization.gson.gson
-import no.uio.ifi.in2000.team37.badeturisten.model.watertemperature.Tsery
+import no.uio.ifi.in2000.team37.badeturisten.data.watertemperature.watertemperature.Tsery
 import no.uio.ifi.in2000.team37.badeturisten.model.watertemperature.WaterTemperatureAPIResponse
 
 class WaterTemperatureDataSource {
@@ -18,7 +18,7 @@ class WaterTemperatureDataSource {
     suspend fun getData(latitude: Double, longitude: Double, maxDistance: Int, maxResultCount: Int): List<Tsery> {
         val timeFrame = "2023-06-01T00:00:00Z/2023-08-01T23:59:59Z"
         // val response = client.get("https://havvarsel-frost.met.no/api/v1/obs/badevann/get?incobs=true&time=latest&nearest=%7B%22maxdist%22%3A${maxDistance}%2C%22maxcount%22%3A${maxResultCount}%2C%22points%22%3A%5B%7B%22lon%22%3A${longitude}%2C%22lat%22%3A${latitude}%7D%5D%7D")
-        //    .body<FrostJsonResponse>()
+        //    .body<WaterTemperatureAPIResponse>()
 
         val response = client.get("https://havvarsel-frost.met.no/api/v1/obs/badevann/get?incobs=true&time=2023-06-01T00%3A00%3A00Z%2F2023-08-01T23%3A59%3A59Z&nearest=%7B%22maxdist%22%3A10%2C%22maxcount%22%3A50%2C%22points%22%3A%5B%7B%22lon%22%3A10.74%2C%22lat%22%3A59.91%7D%5D%7D")
             .body<WaterTemperatureAPIResponse>()
