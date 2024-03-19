b39f0187dfa75308dfbaaa2dbdd8cf563adfc6be
Dan Thanh Ngu <dantn@ulrik.uio.no>
2024-03-19 10:16:58 +0100

    Oppretter filer
    Oppretter BottomBar fil og tilhørende skjermer hvis de ikke eksisterer

---
+++ app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/ui/components/BottomBar.kt
@@ -0,0 +1,8 @@
+package no.uio.ifi.in2000.team37.badeturisten.ui.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BottomBar(){
+    
+}
---
+++ app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/ui/screen/FavouritesScreen.kt
@@ -0,0 +1,7 @@
+package no.uio.ifi.in2000.team37.badeturisten.ui.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FavouritesScreen() {
+}
