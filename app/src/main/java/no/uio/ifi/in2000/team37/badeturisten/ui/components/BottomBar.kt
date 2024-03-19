cc55a96ce431d6079dca9ec25dc97bc336455184
Dan Thanh Ngu <dantn@ulrik.uio.no>
2024-03-19 21:48:53 +0100

    Update BottomBar.kt
    Implementer bottom bar og endre parametere slik at den skal ta inn en skjerm for å sette den som content

--- app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/ui/components/BottomBar.kt
+++ app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/ui/components/BottomBar.kt
@@ -1,8 +1,50 @@
 package no.uio.ifi.in2000.team37.badeturisten.ui.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun BottomBar(){
-    
+fun BottomBar(content: @Composable() () -> Unit){
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(Icons.Filled.Home, contentDescription = "Home")
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.Favorite,
+                            contentDescription = "Favorite",
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.Search,
+                            contentDescription = "Search",
+                        )
+                    }
+                }
+            )
+        },
+    ) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            content
+        }
+    }
 }
