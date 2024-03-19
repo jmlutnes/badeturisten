3b1198b7e37f27ad26252e836155c2340d0eef7d
Lan Anh Tran <lanatr@ulrik.uio.no>
2024-03-19 11:22:35 +0100

    Oppretter et kort med navnet på stranden I BeachProfile

--- app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/ui/screen/BeachProfile.kt
+++ app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/ui/screen/BeachProfile.kt
@@ -1,26 +1,43 @@
 package no.uio.ifi.in2000.team37.badeturisten.ui.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import no.uio.ifi.in2000.team37.badeturisten.ui.components.beachCard
 import no.uio.ifi.in2000.team37.badeturisten.ui.viewmodel.BeachViewModel
 import no.uio.ifi.in2000.team37.badeturisten.ui.viewmodel.HomeViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BeachProfile(beachViewModel: BeachViewModel = viewModel(), navController : NavController, beachName : String?) {
+fun BeachProfile(beachViewModel: BeachViewModel = viewModel(), navController: NavController, beachName: String?) {
     val beach = beachViewModel.beachUIState.collectAsState().value
 
     Scaffold(
@@ -34,12 +51,33 @@ fun BeachProfile(beachViewModel: BeachViewModel = viewModel(), navController : N
                 }
             )
         }
-    ) {paddingValues ->
-        LazyColumn(modifier = Modifier.padding(paddingValues)){
-            item { beach.beach?.let { Text(text = it.name) } }
+    ) { paddingValues ->
+        LazyColumn(modifier = Modifier.padding(paddingValues)) {
+            item {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    color = MaterialTheme.colorScheme.primaryContainer,
+                ) {
+                    Card(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxWidth(),
+                    ) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier
+                                .background(color = Color.White)
+                                .padding(100.dp)
+                                .width(300.dp)
+                                .height(80.dp)
+                        ) {
+                            beach.beach?.let { Text(text = it.name) }
+                        }
+                    }
+                }
+            }
         }
-
     }
-
-
 }
+
