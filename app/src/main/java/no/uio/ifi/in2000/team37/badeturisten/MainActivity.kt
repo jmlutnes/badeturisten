3f90488951a19bbdc46654193172126090d2c9f7
Lan Anh Tran <lanatr@ulrik.uio.no>
2024-04-02 21:09:51 +0200

    Endrer navcontroller og buttombar til NavigationBar (Material 3)

--- app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/MainActivity.kt
@@ -7,18 +7,33 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.vector.ImageVector
+import no.uio.ifi.in2000.team37.badeturisten.ui.components.Screens
 import no.uio.ifi.in2000.team37.badeturisten.ui.favourites.FavouritesScreen
-import no.uio.ifi.in2000.team37.badeturisten.ui.screen.BeachProfile
-import no.uio.ifi.in2000.team37.badeturisten.ui.screen.HomeScreen
+import no.uio.ifi.in2000.team37.badeturisten.ui.beachprofile.BeachProfile
+import no.uio.ifi.in2000.team37.badeturisten.ui.home.HomeScreen
 import no.uio.ifi.in2000.team37.badeturisten.ui.search.SearchScreen
 import no.uio.ifi.in2000.team37.badeturisten.ui.theme.BadeturistenTheme
 
@@ -33,13 +48,104 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    NavScreen()
+                    BottomNavigationBar()
                 }
             }
         }
     }
 }
 
+data class BottomNavigationItem(
+    val label : String = "",
+    val icon : ImageVector = Icons.Filled.Home,
+    val route : String = ""
+) {
+    fun bottomNavigationItems() : List<BottomNavigationItem> {
+        return listOf(
+            BottomNavigationItem(
+                label = "Home",
+                icon = Icons.Filled.Home,
+                route = Screens.Home.route
+            ),
+            BottomNavigationItem(
+                label = "Search",
+                icon = Icons.Filled.Search,
+                route = Screens.Search.route
+            ),
+            BottomNavigationItem(
+                label = "Favourite",
+                icon = Icons.Filled.Favorite,
+                route = Screens.Profile.route
+            ),
+        )
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun BottomNavigationBar() {
+    val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            NavigationBar {
+                BottomNavigationItem().bottomNavigationItems().forEachIndexed { _, navigationItem ->
+                    NavigationBarItem(
+                        selected = navigationItem.route == currentDestination?.route,
+                        label = {
+                            Text(navigationItem.label)
+                        },
+                        icon = {
+                            Icon(
+                                navigationItem.icon,
+                                contentDescription = navigationItem.label
+                            )
+                        },
+                        onClick = {
+                            navController.navigate(navigationItem.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    ) { paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = Screens.Home.route,
+            modifier = Modifier.padding(paddingValues = paddingValues)
+        ) {
+            composable(
+                route = "beachProfile/{beachName}",
+                arguments = listOf(navArgument("beachName") { type = NavType.StringType })
+            ) { backStackEntry ->
+                val beachName = backStackEntry.arguments?.getString("beachName")
+                BeachProfile(navController = navController, beachName = beachName)
+            }
+
+            composable(route = "homeScreen") {
+                HomeScreen(navController = navController)
+            }
+
+            composable(route = "favoritesScreen") {
+                FavouritesScreen(navController = navController)
+            }
+            composable(route = "searchScreen") {
+                SearchScreen(navController = navController)
+            }
+        }
+    }
+}
+
+
+/*
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun NavScreen() {
@@ -66,3 +172,6 @@ fun NavScreen() {
         }
     }
 }
+
+
+ */
