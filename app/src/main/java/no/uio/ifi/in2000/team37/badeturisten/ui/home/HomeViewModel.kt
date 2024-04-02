3fade97e964ba6bdb94969cca8e891fd88e7c6c7
Jöhan Markus Ludvigsen Utnes <jmutnes@ulrik.uio.no>
2024-04-02 14:59:53 +0200

    Update HomeViewModel.kt

--- app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/ui/home/HomeViewModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/ui/home/HomeViewModel.kt
@@ -1,39 +1,44 @@
-package no.uio.ifi.in2000.team37.badeturisten.ui.home
+package no.uio.ifi.in2000.team37.badeturisten.ui.viewmodel
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import no.uio.ifi.in2000.team37.badeturisten.data.metalerts.MetAlertsDataSource
-import no.uio.ifi.in2000.team37.badeturisten.data.metalerts.MetAlertsRepository
-import no.uio.ifi.in2000.team37.badeturisten.data.metalerts.WeatherWarning
-import no.uio.ifi.in2000.team37.badeturisten.data.beach.BeachRepository
+import no.uio.ifi.in2000.team37.badeturisten.data.OsloKommune.OsloKommuneDatasource
+import no.uio.ifi.in2000.team37.badeturisten.data.OsloKommune.OsloKommuneRepository
 import no.uio.ifi.in2000.team37.badeturisten.data.locationforecast.LocationForecastDataSource
 import no.uio.ifi.in2000.team37.badeturisten.data.locationforecast.LocationForecastRepository
+import no.uio.ifi.in2000.team37.badeturisten.data.watertemperature.WaterTemperatureDataSource
+import no.uio.ifi.in2000.team37.badeturisten.data.watertemperature.WaterTemperatureRepository
 import no.uio.ifi.in2000.team37.badeturisten.model.beach.Beach
 import no.uio.ifi.in2000.team37.badeturisten.model.locationforecast.ForecastNextHour
 
-data class MetAlertsUIState(
-    val alerts: List<WeatherWarning> = listOf()
-)
-data class BeachesUIState (
-    val beaches: List<Beach> = listOf()
-)
+data class kommuneBeachList(val beachList: List<Beach> = listOf())
+
 data class ForecastUIState(
     val forecastNextHour: ForecastNextHour? = null
 )
 
-@RequiresApi(Build.VERSION_CODES.O)
-class HomeViewModel: ViewModel() {
+class HomeViewModel(): ViewModel() {
 
-    //henter vaer melding
-    private val _locationForecastRepository : LocationForecastRepository = LocationForecastRepository(dataSource = LocationForecastDataSource())
-    val forecastState: StateFlow<ForecastUIState> = _locationForecastRepository.observeForecastNextHour()
+    //REPOSITORIES
+    private val locationForecastRepository : LocationForecastRepository = LocationForecastRepository(dataSource = LocationForecastDataSource())
+    private val osloKommuneRepository = OsloKommuneRepository (
+        OsloKommuneDatasource()
+    )
+
+    //STATEFLOW
+    private val _kommuneBeachList = MutableStateFlow(kommuneBeachList())
+
+    val kommuneBeachList: StateFlow<kommuneBeachList> = _kommuneBeachList.asStateFlow()
+    val forecastState: StateFlow<ForecastUIState> = locationForecastRepository.observeForecastNextHour()
         .map { ForecastUIState(forecastNextHour = it) }
         .stateIn(
             viewModelScope,
@@ -43,40 +48,10 @@ class HomeViewModel: ViewModel() {
 
     init {
         viewModelScope.launch {
-            _locationForecastRepository.loadForecastNextHour()
-        }
-    }
-
-    //henter strender
-    //trenger en annen maate aa hente alle strender paa
-    private val _beachesRepository = BeachRepository()
-    val beachesState: StateFlow<BeachesUIState> = _beachesRepository.getBeachObservations()
-        .map { BeachesUIState(beaches = it) }
-        .stateIn(
-            viewModelScope,
-            started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = BeachesUIState()
-        )
-
-    init {
-        viewModelScope.launch {
-            _beachesRepository.loadBeaches()
-        }
-    }
-
-    //henter farevarsler
-    private val _metAlertsRepository = MetAlertsRepository(MetAlertsDataSource())
-    val metAlertsState: StateFlow<MetAlertsUIState> = _metAlertsRepository.getMetAlertsObservations()
-        .map { MetAlertsUIState(alerts = it) }
-        .stateIn(
-            viewModelScope,
-            started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = MetAlertsUIState()
-        )
-
-    init {
-        viewModelScope.launch {
-            _metAlertsRepository.getWeatherWarnings()
+            locationForecastRepository.loadForecastNextHour()
+            _kommuneBeachList.update{
+                kommuneBeachList(osloKommuneRepository.makeBeaches(0.0,0.0))
+            }
         }
     }
 }
