aabade2cab5185e5abd6dddcf20909849cd65e6b
Jöhan Markus Ludvigsen Utnes <jmutnes@ulrik.uio.no>
2024-04-02 15:04:28 +0200

    Update BadevannsInfo.kt

--- app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/model/beach/BadevannsInfo.kt
+++ app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/model/beach/BadevannsInfo.kt
@@ -1,6 +1,4 @@
 package no.uio.ifi.in2000.team37.badeturisten.model.beach
 
-data class BadevannsInfo(
-    val generellInfo: String?,
-    val kvalitetInfo: String?
+data class BadevannsInfo(val generellInfo: String, val kvalitetInfo: String, val title: String
 )
