607fa248f9981d496536ef62c3b62add0787fdbd
Dan Thanh Ngu <dantn@ulrik.uio.no>
2024-04-03 15:13:30 +0200

    Endrer favoritt- screen og viewmodel
    Endrer favourite screen og viewmodel

--- app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/ui/favourites/FavouritesViewModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/ui/favourites/FavouritesViewModel.kt
@@ -1,8 +1,5 @@
 package no.uio.ifi.in2000.team37.badeturisten.ui.favourites
 
-import android.os.Build
-import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.SharingStarted
@@ -11,46 +8,31 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import no.uio.ifi.in2000.team37.badeturisten.data.beach.BeachRepository
-import no.uio.ifi.in2000.team37.badeturisten.data.favourite.FavouriteRepository
 import no.uio.ifi.in2000.team37.badeturisten.data.oslokommune.OsloKommuneRepository
 import no.uio.ifi.in2000.team37.badeturisten.model.beach.Beach
 
 data class FavouritesUIState(
     val favourites: List<Beach> = listOf()
 )
-@RequiresApi(Build.VERSION_CODES.O)
 class FavouritesViewModel: ViewModel() {
-/*
     private val _beachRepository: BeachRepository = BeachRepository()
     private val _osloKommuneRepository: OsloKommuneRepository = OsloKommuneRepository()
 
-    val favouritesState: StateFlow<FavouritesUIState> = _beachRepository.getFavouriteObservations()
+    val favouritesState: StateFlow<FavouritesUIState> = _beachRepository.getBeachObservations()
         .map { FavouritesUIState(favourites = it) }
         .stateIn(
             viewModelScope,
             started = SharingStarted.WhileSubscribed(5_000),
             initialValue = FavouritesUIState()
         )
-*/
 
-    /*
-    private val _favouriteRepository: FavouriteRepository = FavouriteRepository()
-    val favouritesState: StateFlow<FavouritesUIState> = _favouriteRepository.getObservations()
-        .map { FavouritesUIState(favourites = it) }
-        .stateIn(
-            viewModelScope,
-            started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = FavouritesUIState()
-        )
-    */
+
 
     init {
         viewModelScope.launch {
-            Log.d("FavViMo, ", "viMoSco.lau")
-            /*
-            _beachRepository.updateFavourites(null)
+            _beachRepository.getFavourites()
             //val osloKommuneBeachInfo: List<Beach> = _osloKommuneRepository.getFavourites()
-            */
+
         }
     }
 }
