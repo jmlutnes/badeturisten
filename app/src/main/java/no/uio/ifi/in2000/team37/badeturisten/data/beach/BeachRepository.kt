cb58b244ed9ca5c8d61da1ded59a1e6884cc98da
Dan Thanh Ngu <dantn@ulrik.uio.no>
2024-04-03 15:21:04 +0200

    rebase til master

--- app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/data/beach/BeachRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/data/beach/BeachRepository.kt
@@ -12,25 +12,23 @@ import no.uio.ifi.in2000.team37.badeturisten.data.watertemperature.jsontokotlin.
 import no.uio.ifi.in2000.team37.badeturisten.model.beach.BadevannsInfo
 import no.uio.ifi.in2000.team37.badeturisten.model.beach.Beach
 
-@RequiresApi(Build.VERSION_CODES.O)
 class BeachRepository {
     //henter fra oslo kommune repository
     val osloKommuneRepository: OsloKommuneRepository = OsloKommuneRepository()
 
     //water temp
     private val waterTempDataSource: WaterTemperatureDataSource = WaterTemperatureDataSource()
-
+    @RequiresApi(Build.VERSION_CODES.O)
     suspend fun waterTempGetData(): List<Tsery> {
         return waterTempDataSource.getData(59.91, 10.74, 10, 50)
     }
 
     //flows
     private val beachObservations = MutableStateFlow<List<Beach>>(listOf())
-    private val favouriteObservations = MutableStateFlow<List<Beach>>(listOf())
     //henter flows
     fun getBeachObservations() = beachObservations.asStateFlow()
-    fun getFavouriteObservations() = favouriteObservations.asStateFlow()
     //oppdaterer flows
+    @RequiresApi(Build.VERSION_CODES.O)
     suspend fun loadBeaches() {
         val observationsFromDataSource = waterTempGetData()
 
@@ -50,7 +48,7 @@ class BeachRepository {
                 val waterTemperature = data.observations.first().body.value.toDoubleOrNull() ?: 0.0
                 val position = data.header.extra.pos
 
-                liste.add(Beach(beachName, position, waterTemperature))
+                liste.add(Beach(beachName, position, waterTemperature, false))
             }
 
             return liste
@@ -65,6 +63,7 @@ class BeachRepository {
         return osloKommuneRepository.getVannkvalitetLoc(lat = lat, lon = lon)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     suspend fun getBeach(beachName: String): Beach? {
         //METODE FOR AA HENTE EN STRAND BASERT PAA LOC ELLER NAVN?
         //val observationsFromDataSource = datasource.getData(59.91, 10.74)
@@ -74,34 +73,12 @@ class BeachRepository {
         return beachlist.firstOrNull()
     }
 
-    suspend fun updateFavourites(beach: Beach?): List<Beach> {
-        val observationsFromStateFlow = getFavouriteObservations()
-        var beachlist: List<Beach> = observationsFromStateFlow.value
-        if (beach != null) {
-            beachlist = beachlist.toMutableList()
-            if (beach in beachlist) {
-                beachlist.remove(beach)
-            } else {
-                beachlist.add(beach)
-            }
-            favouriteObservations.update {
-                beachlist
-            }
-        }
-        return beachlist
-        /*// henter liste over strender
+    @RequiresApi(Build.VERSION_CODES.O)
+    suspend fun getFavourites(): List<Beach> {
         val observationsFromDataSource = waterTempGetData()
         var beachlist: List<Beach> = makeBeaches(observationsFromDataSource)
-        beachlist.forEach { beach -> Log.d("BeRepo, getFavourites", "${beach.name}: ${beach.favorite}") }
-        //favourites ble ikke endret paa etter at jeg trykket paa knapp i card
-        Log.d("BeRepo, getFavourites", "for filter: $beachlist")
         beachlist = beachlist.filter { beach -> beach.favorite }
-        //alltid tom etter
-        Log.d("BeRepo, getFavourites", "etter filter: $beachlist")
-        favouriteObservations.update {
-            beachlist
-        }
-        return beachlist*/
+        return beachlist
     }
 
 }
