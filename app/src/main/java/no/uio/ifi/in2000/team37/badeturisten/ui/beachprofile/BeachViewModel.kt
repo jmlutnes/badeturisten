0bb3a568d784058ee282102a24518c9a84e39af7
jmutnes <jmutnes@uio.no>
2024-04-03 20:17:21 +0200

    Endret slik at informasjon igjen kan vises på osloKommune badestedene. Dette hadde blitt forsvunnet.

--- app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/ui/beachprofile/BeachViewModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/ui/beachprofile/BeachViewModel.kt
@@ -1,8 +1,11 @@
 package no.uio.ifi.in2000.team37.badeturisten.ui.viewmodel
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -15,6 +18,7 @@ import no.uio.ifi.in2000.team37.badeturisten.model.beach.Beach
 
 data class BeachUIState(val beach: Beach? = null, val badevannsinfo: BadevannsInfo?)
 
+@RequiresApi(Build.VERSION_CODES.O)
 class BeachViewModel(savedStateHandle : SavedStateHandle): ViewModel() {
     private val beachName: String = checkNotNull(savedStateHandle["beachName"])
     private val beachRepository: BeachRepository = BeachRepository()
@@ -27,30 +31,27 @@ class BeachViewModel(savedStateHandle : SavedStateHandle): ViewModel() {
 
     init { loadBeachInfo() }
 
+    //Gammel som funker
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun loadBeachInfo() {
-        viewModelScope.launch {
+        viewModelScope.launch (Dispatchers.IO) {
             val beachinfo = beachRepository.getBeach(beachName)
             val osloKommuneBeachInfo = osloKommuneRepository.getBeach(beachName)
             val lon = beachinfo?.pos?.lat?.toDouble()
             val lat = beachinfo?.pos?.lon?.toDouble()
             println("lon:$lon \nlat:$lat")
 
-/*            if (lat != null && lon != null) {
-            val vannkvalitet: BadevannsInfo? = osloKommuneRepository.finnNettside(beachName)*/
-
-            if (lat != null && lon != null) {
-                val vannkvalitet: BadevannsInfo? = _beachRepository.getVannkvalitet(lat, lon)
-                _beachUIState.update { currentUIState ->
-                    if (beachinfo != null) {
-                        currentUIState.copy(beach = beachinfo, badevannsinfo = vannkvalitet)
-                    } else {
-                        currentUIState.copy(
-                            beach = osloKommuneBeachInfo,
-                            badevannsinfo = vannkvalitet
-                        )
-                    }
+            //if (lat != null && lon != null) {
+            val vannkvalitet: BadevannsInfo? = osloKommuneRepository.finnNettside(beachName)
+            _beachUIState.update { currentUIState ->
+                if(beachinfo!=null){
+                    currentUIState.copy(beach = beachinfo, badevannsinfo = vannkvalitet)
+                }
+                else{
+                    currentUIState.copy(beach = osloKommuneBeachInfo, badevannsinfo = vannkvalitet)
                 }
             }
+
         }
     }
 }
