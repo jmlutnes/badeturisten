3e10deebaf3ecc7c6fc349ea9d49493492f69aa0
Dan Thanh Ngu <dantn@ulrik.uio.no>
2024-04-03 15:13:30 +0200

    Endrer favoritt- screen og viewmodel
    Endrer favourite screen og viewmodel

--- app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/ui/favourites/FavouritesScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/ui/favourites/FavouritesScreen.kt
@@ -1,7 +1,6 @@
 package no.uio.ifi.in2000.team37.badeturisten.ui.favourites
 
 import android.os.Build
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
@@ -17,6 +16,13 @@ import no.uio.ifi.in2000.team37.badeturisten.ui.components.beachCard
 @OptIn(ExperimentalFoundationApi::class)
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
+<<<<<<< Updated upstream
+fun FavouritesScreen(navController: NavController) {
+    Scaffold ( ) { padding  ->
+        Text(
+            text = "dette er favoritt screen",
+        modifier = Modifier.padding(padding))
+=======
 fun FavouritesScreen(
     favouritesViewModel: FavouritesViewModel = viewModel(),
     navController: NavController
@@ -30,8 +36,8 @@ fun FavouritesScreen(
         flingBehavior = rememberSnapFlingBehavior(lazyListState = state)
     ) {
         items(favouritesState.favourites) { beach ->
-            Log.d("FavoScreen, cards", "$beach")
             beachCard(beach = beach, navController = navController)
         }
+>>>>>>> Stashed changes
     }
 }
