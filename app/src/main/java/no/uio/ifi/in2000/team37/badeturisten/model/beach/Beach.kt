ce59cd59927728308f177c833b8ecff5e91cb75f
Dan Thanh Ngu <dantn@ulrik.uio.no>
2024-04-08 14:37:19 +0200

    Rebase onto master
    Rebase onto master

--- app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/model/beach/Beach.kt
+++ app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/model/beach/Beach.kt
@@ -4,11 +4,14 @@ import no.uio.ifi.in2000.team37.badeturisten.data.watertemperature.jsontokotlin.
 data class Beach(
     val name: String,
     val pos: Pos,
-    val waterTemp: Double?,
-    var favorite: Boolean
+    val waterTemp: Double?
     /*
     val badevannInfo: BadevannInfo
     val airTemp: Double?
     val img: String
     */
-)
+) {
+    var favorite: Boolean = false
+    /*var firstNameNew = favorite
+        get() = "true"*/
+}
