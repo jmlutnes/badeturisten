95695a0ad6b61179c9f1ded78a7bfde3340ae1c4
Bleron Strana <blerons@ulrik.uio.no>
2024-04-17 09:56:34 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "no.uio.ifi.in2000.team37.badeturisten"
-        minSdk = 24
+        minSdk = 25
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
