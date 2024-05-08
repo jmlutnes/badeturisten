bf3e57357a39c6ed0f6b4fba84a8927061658757
Dan Thanh Ngu <dantn@ulrik.uio.no>
2024-05-08 13:20:28 +0200

    oppdaterer material3 og junit versjon i dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,7 +74,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    val junit_version = "1.6.7"
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$junit_version")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
