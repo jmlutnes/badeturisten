45e5f4a2df1d3d11c2c0fff7790474d8d84b9f0a
Jöhan Markus Ludvigsen Utnes <jmutnes@ulrik.uio.no>
2024-05-13 12:43:23 +0200

    Revert "Nettverks varsel"

--- app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/data/enturjourneyplanner/EnTurJourneyPlannerRepositoryImp.kt
+++ app/src/main/java/no/uio/ifi/in2000/team37/badeturisten/data/enturjourneyplanner/EnTurJourneyPlannerRepositoryImp.kt
@@ -1,16 +1,14 @@
 package no.uio.ifi.in2000.team37.badeturisten.data.enturjourneyplanner
 
-import android.util.Log
 import no.uio.ifi.in2000.team37.badeturisten.data.enturjourneyplanner.jsontokotlinenturjourneyplanner.jsontokotlinenturjourneyplanner
-import no.uio.ifi.in2000.team37.badeturisten.ui.beachprofile.BusRoute
 import no.uio.ifi.in2000.team37.badeturisten.domain.EnTurJourneyPlannerRepository
 import no.uio.ifi.in2000.team37.badeturisten.model.enTur.Busstation
+import no.uio.ifi.in2000.team37.badeturisten.ui.beachprofile.BusRoute
 import javax.inject.Inject
 
 class EnTurJourneyPlannerRepositoryImp @Inject constructor(
     private val datasource: EnTurJourneyPlannerDataSource,
 ) : EnTurJourneyPlannerRepository {
-
     override suspend fun fetchBusroutesById(
         busstationId: String,
         busstation: Busstation,
@@ -19,30 +17,27 @@ class EnTurJourneyPlannerRepositoryImp @Inject constructor(
 
         return try {
             // fetch planning data based on buss station ID
-
-            val routeData: jsontokotlinenturjourneyplanner? = datasource.getRoute(busstationId)
-            if (routeData != null) {
-                if (routeData.data.stopPlace.estimatedCalls.isEmpty() && routeData.data.stopPlace.transportMode[0] != "bus") {
-                    val line = routeData.data.stopPlace
+            val routeData: jsontokotlinenturjourneyplanner = datasource.getRoute(busstationId)
+            if (routeData.data.stopPlace.estimatedCalls.isEmpty() && routeData.data.stopPlace.transportMode[0] != "bus") {
+                val line = routeData.data.stopPlace
+                lines.add(
+                    BusRoute(
+                        null, line.name, line.transportMode[0], busstation
+                    )
+                )
+            } else {
+                routeData.data.stopPlace.estimatedCalls.forEach { estimatedCall ->
+                    val line = estimatedCall.serviceJourney.journeyPattern.line
                     lines.add(
                         BusRoute(
-                            null, line.name, line.transportMode[0], busstation
+                            line.publicCode, line.name, line.transportMode, busstation
                         )
                     )
-                } else {
-                    routeData.data.stopPlace.estimatedCalls.forEach { estimatedCall ->
-                        val line = estimatedCall.serviceJourney.journeyPattern.line
-                        lines.add(
-                            BusRoute(
-                                line.publicCode, line.name, line.transportMode, busstation
-                            )
-                        )
-                    }
                 }
             }
             lines
         } catch (e: Exception) {
-            e.message?.let { Log.d("journeyplanner", it) }
+            println("En feil oppstod ved henting av bussruter: ${e.message}")
             null
         }
     }
